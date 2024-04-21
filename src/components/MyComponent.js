bff5eb7322f0deee597a69876d578b727ddf1620
honganhdinh <honganhdinh.hcm@gmail.com>
2024-04-21 09:42:53 +0700

    commit

---
+++ src/components/MyComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>Hello, world</div>
+        );
+
+    }
+}
+export default MyComponent;
+
