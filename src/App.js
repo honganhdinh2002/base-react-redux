1145c41d19449d613f394bd7d70129967f259f6d
honganhdinh <honganhdinh.hcm@gmail.com>
2024-04-21 10:04:40 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
+import React from 'react';
 import MyComponent from './components/MyComponent';
 
-const App = () => {
-  return (
-    <div>
-      <MyComponent />
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    name: "Howl",
+    address: "288 Nam Ki Khoi Nghia",
+    age: 21
+  }
+  
+  render() {
+    return (
+      <div>
+        My name is {this.state.name} , age is {this.state.age} and i am from {this.state.address}
+
+      </div>
+    )
+  }
 }
+
 
 export default App;
