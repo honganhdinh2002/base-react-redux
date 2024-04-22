e3fed67a0666e517360aa4ceea4c7eb989272fa9
honganhdinh <honganhdinh.hcm@gmail.com>
2024-04-22 09:57:06 +0700

    show hide list users

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -7,8 +7,8 @@ class DisplayInfo extends React.Component {
     }
 
     handleShowHide = () => {
-        this.setState = ({
-            isShowListUser: false
+        this.setState({
+            isShowListUser: !this.state.isShowListUser
         })
     }
     render() {
@@ -18,25 +18,28 @@ class DisplayInfo extends React.Component {
 
         return (
             <div>
-                {listUsers.map((user, index) => {
-                    return (
-                        <div>
-                            <div>
-                                <span onClick={() => { this.handleShowHide() }}>Hide list user</span>
+                <div>
+                    <span onClick={() => { this.handleShowHide() }}>
+                    {this.state.isShowListUser === true ? "Hide list user": "Show list user"}
+                    </span>
+                </div>
+                <div>
+                    {listUsers.map((user, index) => {
+                        return (
+                            <div key={user.id} >
+                                {this.state.isShowListUser &&
+                                    <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                                        <div>My name's {user.name}</div>
+                                        <div>My age's {user.age}</div>
+                                        <hr />
+                                    </div>
+                                }
+
                             </div>
+                        )
 
-                            {this.state.isShowListUser &&
-                                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                                    <div>My name's {user.name}</div>
-                                    <div>My age's {user.age}</div>
-                                    <hr />
-                                </div>
-                            }
-
-                        </div>
-                    )
-
-                })}
+                    })}
+                </div>
             </div>
         )
     }
