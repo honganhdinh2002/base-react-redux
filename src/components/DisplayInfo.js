b082d394166bdadeb57ad7153b1ce0b953d4a220
honganhdinh <honganhdinh.hcm@gmail.com>
2024-04-22 09:39:01 +0700

    commit

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -2,18 +2,41 @@ import React from "react";
 
 
 class DisplayInfo extends React.Component {
+    state = {
+        isShowListUser: true
+    }
+
+    handleShowHide = () => {
+        this.setState = ({
+            isShowListUser: false
+        })
+    }
     render() {
-        const {listUsers} = this.props;
-        return(
+        const { listUsers } = this.props;
+        console.log(listUsers);
+        console.table(listUsers);
+
+        return (
             <div>
-                { listUsers.map((user, index) => {
-                    return(
-                        <div key={user.id}>
-                            <p> My name's {user.name} </p>
-                            <p> My age's {user.age} </p>
+                {listUsers.map((user, index) => {
+                    return (
+                        <div>
+                            <div>
+                                <span onClick={() => { this.handleShowHide() }}>Hide list user</span>
+                            </div>
+
+                            {this.state.isShowListUser &&
+                                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                                    <div>My name's {user.name}</div>
+                                    <div>My age's {user.age}</div>
+                                    <hr />
+                                </div>
+                            }
+
                         </div>
                     )
-                }) }
+
+                })}
             </div>
         )
     }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,19 +6,19 @@ class MyComponent extends React.Component {
   state = {
     listUsers: [
       {id: 1, name: "Howl", age: 21},
-      {id: 2, name: "Anh", age: 21},
-      {id: 3, name: "Popp", age: 21},
+      {id: 2, name: "Anh", age: 16},
+      {id: 3, name: "Popp", age: 23},
 
     ]
   }
     render() {
         return (
           <div>
-            <DisplayInfo listUsers = {this.state.listUsers} />
-            
-            <br/>
 
             <UserInfo />
+            <br/>
+            <DisplayInfo listUsers = {this.state.listUsers} />
+            
           </div>
         )
       }
