1c3fea0597975cf4eb5500a611b7ccef598f2437
honganhdinh <honganhdinh.hcm@gmail.com>
2024-05-02 15:06:01 +0700

    outlet

--- src/index.js
+++ src/index.js
@@ -4,13 +4,24 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import User from './components/User/User';
+import Admin from './components/Admin/Admin';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(   
   <Provider store={store}>
     {/* <React.StrictMode> */}
-      <App />
+    <BrowserRouter>
+    <Routes>  
+      <Route path='/' element={<App />}>
+        <Route path='/users' element={<User />} />
+        <Route path='/admins' element={<Admin />} />
+      </Route>
+      
+    </Routes>
+    </BrowserRouter>
     {/* </React.StrictMode> */}
   </Provider>
 );
