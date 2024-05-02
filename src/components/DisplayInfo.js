2bb231bee19d3326a4966cf99a690e592cd960b4
honganhdinh <honganhdinh.hcm@gmail.com>
2024-05-02 12:11:29 +0700

    change state and src

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,72 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import './DisplayInfo.scss';
-import logo from "./../logo.svg";
 
 
 
-// class DisplayInfo extends React.Component {
-//     render() {
-//         console.log("Render");
-//         const name = "Hoi Dan IT";
-//         const { listUsers } = this.props;
-
-//         return (
-//             <div className="display-info-conatainer">
-//                 { name }
-//                 <img src={logo} className="logo" />
-//                     <div>
-//                     {listUsers.map((user, index) => {
-//                         return (
-//                             <div key={user.id} >
-//                                 {true &&
-//                                     <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-//                                         <div>
-//                                         Họ và tên: {user.name}</div>
-//                                         <div>Tuổi:{user.age}</div>
-//                                         <div>Địa chỉ: {user.address}</div>
-//                                         <button onClick={() => this.props.handleDeleteUser(user.id)}>Delete</button>
-//                                         <hr />
-//                                     </div>
-//                                 }
-//                             </div>
-//                         )
-
-//                     })}
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
-
-
-
-// Stateless vs Statefull
 const DisplayInfo = (props) => {
-        const { listUsers } = props;
+    const { listUsers } = props;
+    const [isActive, setActive] = useState(true);
 
-        return (
-            <div className="display-info-conatainer">
-                <img src={logo} className="logo" />
-                    <div>
-                    {listUsers.map((user, index) => {
-                        return (
-                            <div key={user.id} >
-                                {true &&
-                                    <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                                        <div>
-                                        Họ và tên: {user.name}</div>
-                                        <div>Tuổi:{user.age}</div>
-                                        <div>Địa chỉ: {user.address}</div>
-                                        <button onClick={() => props.handleDeleteUser(user.id)}>Delete</button>
-                                        <hr />
-                                    </div>
-                                }
-                            </div>
-                        )
+    const handleActive = () => {
+        setActive(!isActive);
+    }
 
-                    })}
-                </div>
+    return (
+        <div className="display-info-conatainer">
+            <div>
+                <span onClick={() => handleActive()}>
+                    {isActive === true ? "Hide" : "Show"}
+                </span>
             </div>
-        )
+
+            <div>
+                {listUsers.map((user, index) => {
+                    return (
+                        <div key={user.id} >
+                            {isActive &&
+                                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                                    <div>Họ và tên: {user.name}</div>
+                                    <div>Tuổi:{user.age}</div>
+                                    <div>Địa chỉ: {user.address}</div>
+                                    <button onClick={() => props.handleDeleteUser(user.id)}>Delete</button>
+                                    <hr />
+                                </div>
+                            }
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
 }
 export default DisplayInfo;
